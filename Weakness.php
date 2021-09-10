3d922dd9ebe9c86afcfde1845ff17a3881e73ac5
Diony <dionybusker@gmail.com>
2021-09-10 11:48:46 +0200

    Added class "Weakness"

---
+++ Weakness.php
@@ -0,0 +1,11 @@
+<?php
+
+class Weakness {
+    public $name;
+    public $multiplier;
+
+    public function __construct($name, $multiplier) {
+        $this->name = $name;
+        $this->multiplier = $multiplier;
+    }
+}
