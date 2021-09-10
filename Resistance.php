420b3b53ae8062ac63d4c13d77054804dddd0239
Diony <dionybusker@gmail.com>
2021-09-10 11:49:04 +0200

    Added class "Resistance"

---
+++ Resistance.php
@@ -0,0 +1,11 @@
+<?php
+
+class Resistance {
+    public $name;
+    public $value;
+
+    public function __construct($name, $value) {
+        $this->name = $name;
+        $this->value = $value;
+    }
+}
