c81dab9673723cbcf5733dff5fb1c3cb14587b05
Diony <dionybusker@gmail.com>
2021-09-10 11:48:29 +0200

    Added class "EnergyType", using constants

---
+++ EnergyType.php
@@ -0,0 +1,16 @@
+<?php
+
+class EnergyType {
+    const LIGHTNING = "Lightning";
+    const FIGHTING = "Fighting";
+    const FIRE = "Fire";
+    const WATER = "Water";
+
+    public $name;
+    public $value;
+
+    public function __construct($name, $value) {
+        $this->name = $name;
+        $this->value = $value;
+    }
+}
