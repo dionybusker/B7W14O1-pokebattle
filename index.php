3b6f1f60aa9591a36d24197f25031f837eae16be
Diony <dionybusker@gmail.com>
2021-09-14 11:02:28 +0200

    For loop to loop through the attacks

--- index.php
+++ index.php
@@ -9,3 +9,13 @@ $charmeleon = new Charmeleon("Charmeleon");
 
 print_r("<pre>" . $pikachu . "</pre>");
 print_r("<pre>" . $charmeleon . "</pre>");
+
+for ($i = 0; $i < 2; $i++) {
+    if ($pikachu->attacks[$i]->name == "Electric Ring") {
+        $pikachu->attack($pikachu, $pikachu->attacks[$i], $charmeleon);
+    }
+
+    if ($charmeleon->attacks[$i]->name == "Flare") {
+        $charmeleon->attack($charmeleon, $charmeleon->attacks[$i], $pikachu);
+    }
+}
