8e0224f8dc7a4c61cbc3cab8f51b8cebfcfe83ce
Diony <dionybusker@gmail.com>
2021-09-21 09:54:38 +0200

    Added fight in for loop, print details

--- index.php
+++ index.php
@@ -4,18 +4,36 @@ require "Pokemon.php";
 require "Pikachu.php";
 require "Charmeleon.php";
 
+echo "<h3>Pokémon</h3>";
 $pikachu = new Pikachu("Pikachu");
 $charmeleon = new Charmeleon("Charmeleon");
 
 print_r("<pre>" . $pikachu . "</pre>");
 print_r("<pre>" . $charmeleon . "</pre>");
 
-for ($i = 0; $i < 2; $i++) {
-    if ($pikachu->attacks[$i]->name == "Electric Ring") {
-        $pikachu->attack($pikachu, $pikachu->attacks[$i], $charmeleon);
-    }
+Pokemon::getPopulation();
 
-    if ($charmeleon->attacks[$i]->name == "Flare") {
-        $charmeleon->attack($charmeleon, $charmeleon->attacks[$i], $pikachu);
+echo "<h3>Fight</h3>";
+for ($loop = 0; $loop < 2; $loop++) {
+    echo "<strong>Attack #" . ($loop + 1) . "</strong> <br>";
+    for ($i = 0; $i < 2; $i++) {
+        if ($pikachu->health > 0) {
+            if ($pikachu->attacks[$i]->name == "Electric Ring") {
+                $pikachu->attack($pikachu, $pikachu->attacks[$i], $charmeleon);
+            }
+        } else {
+            unset($pikachu);
+        }
+    
+        if ($charmeleon->health > 0) {
+            if ($charmeleon->attacks[$i]->name == "Flare") {
+                $charmeleon->attack($charmeleon, $charmeleon->attacks[$i], $pikachu);
+            }
+        } else {
+            unset($charmeleon);
+        }
     }
 }
+
+echo "<h3>Statistics</h3>";
+Pokemon::getPopulation();
